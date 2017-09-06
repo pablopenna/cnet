2aef8aaabfa8131e08d0bb4ec87e57b9dc301ff7
pablopenna <p.pena@edu.uah.es>
2017-09-06 14:02:23 +0200

    Se ha mejorado el envio de mensajes del cliente al servidor.

--- Ejemplos_Beejs/client01.c
+++ Ejemplos_Beejs/client01.c
@@ -1,5 +1,5 @@
-/* SERVER
-*/
+/* CLIENT
+ */
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,43 +13,70 @@
 
 int main (void)
 {
-	//Preparar conexiones
-	struct addrinfo hints, *res;
-	int sockfd;
+		//Preparar conexiones
+		struct addrinfo hints, *res;
+		int sockfd;
 
-	//getaddrinfo()
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;	//IPV4 o IPV6
-	hints.ai_socktype = SOCK_STREAM;//TCP
-	
-	int status;
-	status = getaddrinfo(SERVER_ADDR, MYPORT, &hints, &res);
-	printf("getaddrinfo() -> %d\n",status);
-	
+		//getaddrinfo()
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_UNSPEC;	//IPV4 o IPV6
+		hints.ai_socktype = SOCK_STREAM;//TCP
 
-	//make a socket
-	sockfd = socket(res->ai_family ,res->ai_socktype, res->ai_protocol);
+		int status;
+		status = getaddrinfo(SERVER_ADDR, MYPORT, &hints, &res);
+		printf("getaddrinfo() -> %d\n",status);
 
-	//Nos saltamos bind(), pues realmente nos es indiferente que socket utilice el
-	//clinte. Dejamos que el SO escoja uno disponible.
 
-	//connect
+		//make a socket
+		sockfd = socket(res->ai_family ,res->ai_socktype, res->ai_protocol);
 
-	status = connect(sockfd, res->ai_addr, res->ai_addrlen);
-	printf("connect() -> %d\n",status);
-	
-	//mensaje
-	char *msg = "Mensaje del cliente";
-	int len, bytes_sent;
-	
-	//enviar mensaje
-	len = strlen(msg);
-	bytes_sent = send(sockfd, msg, len, 0);
-	printf("Mensaje enviado: %s\nbytes: %d\n",msg,bytes_sent);
-	//recibir mensaje
+		//Nos saltamos bind(), pues realmente nos es indiferente que socket utilice el
+		//clinte. Dejamos que el SO escoja uno disponible.
 
-	//FIN
-	close(sockfd);
+		//connect
 
-	return 0;
+		status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+		printf("connect() -> %d\n",status);
+
+		//mensaje
+		//char *msg = "Mensaje del cliente";
+		int len, bytes_sent;
+
+		//enviar mensaje
+		int max_len = 1024;
+		char mensaje [max_len];
+
+		do
+		{
+				printf("Escribe mensaje: ");
+				//scanf("%s", mensaje);
+				//Utilizamos fgets() para leer solo una linea y poder mandar
+				//mensajes vacios si pulsamos unicamente enter para el input.
+				fgets(mensaje, max_len, stdin);
+
+				//al utilizar fgets(), siempre se almacenara '\n' al final del
+				//mensaje, pues pulsaremos enter para indicar el final del
+				//input. Para eliminar este '\n', utilizaremos la funcion
+				//strcspn(char *s, char *reject), que nos indica cuantos bytes
+				//de s hay hasta que encontramos uno de los componentes de
+				//reject.
+				//Sustituimos final de linea por final de string.
+				mensaje[strcspn(mensaje, "\n")] = 0;
+
+				//len = strlen(msg);
+				//bytes_sent = send(sockfd, msg, len, 0);
+				//printf("Mensaje enviado: %s\nbytes: %d\n",msg,bytes_sent);
+				len = strlen(mensaje);
+				bytes_sent = send(sockfd, mensaje, len, 0);
+				printf("Mensaje enviado: %s\nbytes: %d\n",mensaje,bytes_sent);
+		}while(len > 0);
+
+		//recibir mensaje
+		//TODO
+
+
+		//FIN
+		close(sockfd);
+
+		return 0;
 }
--- Ejemplos_Beejs/server01.c
+++ Ejemplos_Beejs/server01.c
@@ -1,5 +1,5 @@
 /* SERVER
-*/
+ */
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,64 +14,66 @@
 int main (void)
 {
 	struct sockaddr_storage their_addr; 	//se utiliza el struct
-											//sockaddr_storage cuando se
-											//desconoce si se va a utilizar ipv4
-											//o ipv6, ya que en este struct
-											//caben ambas structuras. Más tarde
-											//se casteará al tipo conveniente
+	//sockaddr_storage cuando se
+	//desconoce si se va a utilizar ipv4
+	//o ipv6, ya que en este struct
+	//caben ambas structuras. Más tarde
+	//se casteará al tipo conveniente
 	socklen_t addr_size;
 	struct addrinfo hints, *res;	//Necesarios para getaddrinfo()
 	int sockfd, new_fd;
 
 	//Primero cargamos direcciones con getaddrinfo()
-
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC; //usa ipv4 o ipv6, cualquiera
 	hints.ai_socktype = SOCK_STREAM; //TCP
 	hints.ai_flags = AI_PASSIVE;	//fill in my ip for me. Como somos el
-									//servidor, daremos nuestra ip como conexion	
+	//servidor, daremos nuestra ip como conexion	
 	int status;
 	status = getaddrinfo(NULL, MYPORT, &hints, &res);
 	if(status!=0)
 	{
-		printf("Error en getaddrinfo(): %d\n", status);
+			printf("Error en getaddrinfo(): %d\n", status);
 	}
 	else// FALTA PONER LAS COMPROBACIONES DE ERRORES EN LA LLAMADAS AL SISTEMA
 	{
-		printf("llamada a getaddrinfo() exitosa\n");
+			printf("llamada a getaddrinfo() exitosa\n");
 
-		//make a socket, bind it, and listen on it.
-		
-		//Obtenemos el filedescriptor del socket a utilizar para las conexiones
-		//entrantes
-		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+			//make a socket, bind it, and listen on it.
+			//Obtenemos el filedescriptor del socket a utilizar para las conexiones
+			//entrantes
+			sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-		//Asignamos el puerto MYPORT al socket
-		bind(sockfd, res->ai_addr, res->ai_addrlen);
+			//Asignamos el puerto MYPORT al socket
+			bind(sockfd, res->ai_addr, res->ai_addrlen);
 
-		//Escuchamos conexiones entrantes
-		listen(sockfd, BACKLOG);
+			//Escuchamos conexiones entrantes
+			listen(sockfd, BACKLOG);
 
-		//Ahora podemos aceptar conexiones entrantes.
+			//Ahora podemos aceptar conexiones entrantes.
+			addr_size = sizeof their_addr;
 
-		addr_size = sizeof their_addr;
-		
-		//Este es el socket que utilizaremos paraofrecerle los servicios al
-		//cliente cuya conexion hayamos aceptado.
-		new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size);
+			//Este es el socket que utilizaremos paraofrecerle los servicios al
+			//cliente cuya conexion hayamos aceptado.
+			new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size);
 
-		//Todo listo para utilizar el socket new_fd para comunicarnos
-		int max_len = 1024;
-		char msg [max_len];
-		memset(msg, 0, max_len);
+			//Todo listo para utilizar el socket new_fd para comunicarnos
+			int max_len = 1024;
+			char msg [max_len];
+			memset(msg, 0, max_len);
+			int bytes_rcv;
+			do
+			{
+				memset(msg,0,max_len);
+				bytes_rcv = recv(new_fd, msg, max_len, 0);
+				printf("Mensaje recibido: %s\n%d bytes\n", msg, bytes_rcv);
+			}while(bytes_rcv > 0);
 
-		int bytes_rcv = recv(new_fd, msg, max_len, 0);
-		printf("Mensaje recibido: %s\n%d bytes\n", msg, bytes_rcv);
+			printf("Conexion cerrada por el cliente\n");
 
-		
-		//FINAL
-		close(sockfd);
-		close(new_fd);
+			//FINAL
+			close(sockfd);
+			close(new_fd);
 	}
 
 	return 0;
